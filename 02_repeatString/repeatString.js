fe74d66bcdf605f50eee87c2e8f5328626937306
Aliya Gamez <aliya.e.gamez@gmail.com>
2024-10-27 18:00:29 -0400

    finished two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, repeatInput) {
+    let stringResult = '';
+    if(repeatInput < 0)
+        return 'ERROR';
+    for(let i = 1; i <= repeatInput; i++) {
+        stringResult += stringInput;
+    }
+    return stringResult;
 };
 
 // Do not edit below this line
