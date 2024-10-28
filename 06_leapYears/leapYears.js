6480bf214fd4a45875f85c2cb14a3f69a86a4804
Aliya Gamez <aliya.e.gamez@gmail.com>
2024-10-28 10:55:47 -0400

    completed leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if(yearInput%4==0) {
+        if(yearInput%100==0) {
+            if(yearInput%400==0)
+                return true;
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
