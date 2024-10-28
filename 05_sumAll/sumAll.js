ebc88e06ad106751df546d6360cc4f9fa9f14ac0
Aliya Gamez <aliya.e.gamez@gmail.com>
2024-10-28 10:50:13 -0400

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+// 
 
+const sumAll = function(intInput1, intInput2) {
+    let intInputSum = 0;
+    //input conditions
+    if(intInput1 < 0 || intInput2 < 0) {
+        return 'ERROR';
+    }
+    else if(!Number.isInteger(intInput1) || !Number.isInteger(intInput2)) {
+        return 'ERROR';
+    }
+    //sum conditions
+    if(intInput1 < intInput2) {
+        for(let i = intInput1; i <= intInput2; i++) {
+            intInputSum += i;
+        }
+    }
+    else if(intInput1 > intInput2) {
+        for(let i = intInput2; i <= intInput1; i++) {
+            intInputSum += i;
+        }
+    }
+    return intInputSum;
 };
 
 // Do not edit below this line
